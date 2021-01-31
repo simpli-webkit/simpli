cf90d3844ac5dc32914ecc5d09c629e30bd6a9d4
acol248 <acol248@users.noreply.github.com>
2021-01-31 02:01:19 +0000

    gucci

--- simpli_0-0-2_Beta/core/scripts/navbar.js
+++ simpli_0-0-2_Beta/core/scripts/navbar.js
@@ -10,7 +10,8 @@ function mobileNavbar() {
         $('.section-fixed').css("padding-top", 0);
         $('.cover').css("padding-top", 0);
         $('.fixed-sidebar').css("padding-top", 0);
-        $('.section').css("margin-top", 0);
+        $('.section').css("padding-top", 0);
+        $('.coverpg-section').css("margin-top", 0);
     } else {
       x.style.transform = "scale(1, 1)";
       var clientHeight = document.getElementById('navbarMobile').clientHeight;
@@ -18,7 +19,8 @@ function mobileNavbar() {
       $('.section-fixed').css("padding-top", clientHeight);
       $('.cover').css("padding-top", clientHeight);
       $('.fixed-sidebar').css("padding-top", clientHeight);
-      $('.section').css("margin-top", clientHeight);
+      $('.section').css("padding-top", clientHeight);
+      $('.coverpg-section').css("margin-top", clientHeight);
     }
   }
 
@@ -30,7 +32,8 @@ function mobileNavbarCancel() {
       $('.section-fixed').css("padding-top", 0);
       $('.cover').css("padding-top", 0);
       $('.fixed-sidebar').css("padding-top", 0);
-      $('.section').css("margin-top", 0);
+      $('.section').css("padding-top", 0);
+      $('.coverpg-section').css("margin-top", 0);
   }
 }
 
