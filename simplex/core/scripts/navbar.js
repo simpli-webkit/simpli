313b0a3e875f9c55ca7fc482d4acd85c764e8fe1
Alex Collyer <alex.collyer@bloc.digital>
2021-04-02 11:51:46 +0100

    + navbar mobile transparent fixes

--- simplex/core/scripts/navbar.js
+++ simplex/core/scripts/navbar.js
@@ -30,16 +30,26 @@ function mobileNavbar() {
 
 function mobileNavbarCancel() {
   var x = document.getElementById("navbarMobile");
-  if (x.style.transform === "scale(1, 1)") {
-    x.style.transform = "scale(1, 0)";
-    $('.sitecontent').css("padding-top", 0);
-      if($("nav").hasClass("navbar-transparent")) {
-        $('.navbar-color-transparent-white').removeClass('navbar-color-transparent-white-fill');
-        $('.navbar-color-transparent-red').removeClass('navbar-color-transparent-red-fill');
-        $('.navbar-color-transparent-blue').removeClass('navbar-color-transparent-blue-fill');
-        $('.navbar-color-transparent-purple').removeClass('navbar-color-transparent-purple-fill');
-        $('.navbar-color-transparent-green').removeClass('navbar-color-transparent-green-fill');
-      }
+  var windH = $(window).height(); 
+  triggerH = windH - 220;
+
+  if ($(this).scrollTop() > triggerH) {
+    if (x.style.transform === "scale(1, 1)") {
+      x.style.transform = "scale(1, 0)";
+      $('.sitecontent').css("padding-top", 0);
+    }
+  } else {
+    if (x.style.transform === "scale(1, 1)") {
+      x.style.transform = "scale(1, 0)";
+      $('.sitecontent').css("padding-top", 0);
+        if($("nav").hasClass("navbar-transparent")) {
+          $('.navbar-color-transparent-white').removeClass('navbar-color-transparent-white-fill');
+          $('.navbar-color-transparent-red').removeClass('navbar-color-transparent-red-fill');
+          $('.navbar-color-transparent-blue').removeClass('navbar-color-transparent-blue-fill');
+          $('.navbar-color-transparent-purple').removeClass('navbar-color-transparent-purple-fill');
+          $('.navbar-color-transparent-green').removeClass('navbar-color-transparent-green-fill');
+        }
+    }
   }
 }
 
