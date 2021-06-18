cedca69e0ab971c37575f1b8582202fde6910845
acol248 <63760139+acol248@users.noreply.github.com>
2021-06-18 01:17:11 +0100

    Update pageloader.js

--- simplex/core/scripts/pageloader.js
+++ simplex/core/scripts/pageloader.js
@@ -2,6 +2,14 @@
 AUTHOR: ALEX COLLYER (alex.collyer@getsimpli.co.uk)
 */
 
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
+
 $(document).ready(function () {
     sleep(300);
     var loader = document.getElementById("pageloader");
